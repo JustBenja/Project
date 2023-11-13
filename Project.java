b381e2dc331c1cc6a0e5f7599a05c2a4c99b7a74
JustBenja <arielbinyamin555@gmail.com>
2023-11-13 18:18:15 +0200

    everything

---
+++ Project.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.util.List;
+import java.util.Scanner;
+public class Project {
+    public static void main(String args[]) {
+    }
+}
+interface IamHuman{
+    void EnterName();}
+class Person implements IamHuman{
+    private int Age;
+    private String Name;
+    private ArrayList<Person> familyTree;
+    public Person(int age, String name){
+        this.Age = age;
+        this.Name = name;
+        this.familyTree = new ArrayList<Person>();
+    }
+    public void EnterName() {
+        Scanner input = new Scanner(System.in);
+        this.Name = input.nextLine();
+        this.Age = input.nextInt();
+    }
+}
