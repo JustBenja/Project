a98e2879bdf9f20a70f3a18a4e4a8f93dcca6a6f
JustBenja <arielbinyamin555@gmail.com>
2023-11-13 17:45:24 +0200

    Update Boboclat.java

--- Boboclat.java
+++ Boboclat.java
@@ -1,5 +1,25 @@
-public class Boboclat {
+import java.util.Scanner;
+public class Boboclat{
     public static void main(String args[]){
-
+        Person H = new Person(15, "Ariel", {1,2,3,4,5});
+        System.out.println(Age);
     }
 }
+interface IamHuman{
+    void EnterName();
+}
+    public class Person implements IamHuman{
+        private int Age;
+        private String Name;
+        private List<Person> familyTree;
+        public Person(int age, String name){
+            this.Age = age;
+            this.Name = name;
+            this.familyTree = new List<Person>;
+        }
+        public void EnterName() {
+            Scanner input = new Scanner(System.in);
+            this.Name = input.nextLine();
+            this.Age = input.nextInt();
+            }
+        }
